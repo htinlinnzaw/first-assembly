bedcf2df9a2b1bfd0d551d89ccf9ff2571a33f56
HtinLinnZaw <htinlinnzaw.dev@gmail.com>
2020-11-01 22:29:42 +0630

    1. fix: remove unused function _start

--- first.asm
+++ first.asm
@@ -15,15 +15,6 @@ CMAIN:
     xor rdi, rdi
     syscall
     ret
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 13
-    syscall
-    mov eax, 60
-    xor rdi, rdi
-    syscall
     
 message:
     db "Hello", 10

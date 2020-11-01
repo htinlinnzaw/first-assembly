d6eecf81caacded8ec6af7568d527dc2634ac0b5
HtinLinnZaw <htinlinnzaw.dev@gmail.com>
2020-11-01 22:20:58 +0630

    1. add: HelloWorld output

--- first.asm
+++ first.asm
@@ -2,9 +2,28 @@
 
 section .text
 global CMAIN
-global _start
 CMAIN:
     mov ebp, esp; for correct debugging
     ;write your code here
     xor eax, eax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 13
+    syscall
+    mov eax, 60
+    xor rdi, rdi
+    syscall
     ret
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 13
+    syscall
+    mov eax, 60
+    xor rdi, rdi
+    syscall
+    
+message:
+    db "Hello", 10
